ae14562717fd7b7ac6722ee3226c9d32f433cef3
Leo Carrati <carrati3@gmail.com>
2023-06-19 16:39:12 -0300

    FIX: remover dontenv

--- src/Api/axios.ts
+++ src/Api/axios.ts
@@ -1,18 +1,17 @@
 import axios from "axios";
-import dotenv from 'dotenv'
 
-dotenv.config()
+const BASE_URL = 'https://vps72.heliohost.us/labeddit-backend/'
 
 // para requisições públicas
 export default axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: BASE_URL,
     headers: { "Content-Type": "application/json" },
     withCredentials: true
   });
   
   // para requisições que precisam ser enviadas com Authorization no header
   export const axiosPrivate = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: BASE_URL,
     headers: { "Content-Type": "application/json" },
     withCredentials: true
   });
